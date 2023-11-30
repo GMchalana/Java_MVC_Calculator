9d290b71ef38a1e640690a81ad47b0283dd4ed19
Chalana Prabhashwara <gmchalanaprabhashwara@gmail.com>
2023-11-30 23:45:19 +0530

    finalize
    Finalize the project

---
+++ src/CalculatorModel.java
@@ -0,0 +1,21 @@
+public class CalculatorModel {
+    private float answer;
+    public void addTwoNumbers(float firstNumber, float secondNumber){
+        answer = firstNumber + secondNumber;
+    }
+
+    public void subTwoNumbers(float firstNumber, float secondNumber){
+        answer = firstNumber - secondNumber;
+    }
+
+    public void mulTwoNumbers(float firstNumber, float secondNumber){
+        answer = firstNumber * secondNumber;
+    }
+
+    public void divTwoNumbers(float firstNumber, float secondNumber){
+        answer = firstNumber / secondNumber;
+    }
+    public float getAnswer(){
+        return answer;
+    }
+}
